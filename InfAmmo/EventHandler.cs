a2e2892492dfb98e867df2a45757aad10ea6cf38
Mariki <markarovegor@yandex.ru>
2025-01-04 13:08:38 +0300

    Добавьте файлы проекта.

---
+++ InfAmmo/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Extensions;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfAmmo
+{
+    public class EventHandler
+    {
+        public void RegisterEvents()
+        {
+            Exiled.Events.Handlers.Player.ReloadingWeapon += OnReloading;
+        }
+        public void OnReloading(ReloadingWeaponEventArgs ev)
+        {
+            if (ev.Item.Type == ItemType.ParticleDisruptor) return;
+            ev.Player.Inventory.ServerSetAmmo(ev.Firearm.AmmoType.GetItemType(), 0);
+            ev.Player.AddAmmo(ev.Firearm.AmmoType, 100);
+        }
+        public void UnRegisterEvents()
+        {
+            Exiled.Events.Handlers.Player.ReloadingWeapon -= OnReloading;
+        }
+    }
+}
---
+++ InfAmmo/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfAmmo
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Mariki";
+        public override string Name => "InfAmmo";
+
+        public static EventHandler handler;
+        public override void OnEnabled()
+        {
+            handler = new EventHandler();
+            handler.RegisterEvents();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            handler = new EventHandler();
+            handler.UnRegisterEvents();
+            base.OnDisabled();
+        }
+    }
+}
