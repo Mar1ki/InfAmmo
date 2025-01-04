86ec870a6da2dc43a98cd37ebf96270f54cc1339
Mariki <markarovegor@yandex.ru>
2025-01-04 13:13:04 +0300

    HAHA idk

--- InfAmmo/EventHandler.cs
+++ InfAmmo/EventHandler.cs
@@ -13,17 +13,31 @@ namespace InfAmmo
     {
         public void RegisterEvents()
         {
-            Exiled.Events.Handlers.Player.ReloadingWeapon += OnReloading;
+            Exiled.Events.Handlers.Player.ReloadingWeapon+=OnReloading;
+            Exiled.Events.Handlers.Player.Spawned += OnSpawned;
+            Exiled.Events.Handlers.Player.DroppingAmmo += OnDroppingAmmo;
+        }
+        public void OnDroppingAmmo(DroppingAmmoEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+        public void OnSpawned(SpawnedEventArgs ev)
+        {
+            ev.Player.Inventory.ServerSetAmmo(ItemType.Ammo44cal, 200);
+            ev.Player.Inventory.ServerSetAmmo(ItemType.Ammo12gauge, 200);
+            ev.Player.Inventory.ServerSetAmmo(ItemType.Ammo556x45, 200);
+            ev.Player.Inventory.ServerSetAmmo(ItemType.Ammo762x39, 200);
+            ev.Player.Inventory.ServerSetAmmo(ItemType.Ammo9x19, 200);
         }
         public void OnReloading(ReloadingWeaponEventArgs ev)
         {
             if (ev.Item.Type == ItemType.ParticleDisruptor) return;
-            ev.Player.Inventory.ServerSetAmmo(ev.Firearm.AmmoType.GetItemType(), 0);
-            ev.Player.AddAmmo(ev.Firearm.AmmoType, 100);
+            ev.Player.Inventory.ServerSetAmmo(ev.Firearm.AmmoType.GetItemType(), 200);
         }
         public void UnRegisterEvents()
         {
             Exiled.Events.Handlers.Player.ReloadingWeapon -= OnReloading;
+            Exiled.Events.Handlers.Player.Spawned -= OnSpawned;
         }
     }
 }
